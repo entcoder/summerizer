ba987857f74840dcc39dadf05a1f2440c463b5bd
Chinmay <111702755+entcoder@users.noreply.github.com>
2023-07-10 21:20:43 +0530

    add:initial files

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+
+import "./App.css"
+import "./components/Demo"
+import Hero from './components/Hero'
+import Demo from './components/Demo'
+
+
+const App = () => {
+  return (
+    <main>
+        <div className="main">
+            <div className="gradient" />
+        </div>
+  
+        <div className="app">
+           <Hero/>
+           <Demo/>
+        </div>
+        
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Demo.jsx
@@ -0,0 +1,119 @@
+import React from 'react'
+
+import { useEffect,useState } from 'react'
+import {copy,linkIcon,loader,tick} from '../assets' 
+import {useLazyGetSummaryQuery} from '../services/article'
+
+const Demo = () => {
+
+
+
+     const [article,setArticle] =useState({
+      url:'',
+      summary:'',
+
+     });
+
+     const [allArticles,setAllArticles]=useState([]);
+
+     const [getSummary, {error,isFetching}]=useLazyGetSummaryQuery();
+
+     useEffect(()=>{
+      
+      const articlesFromLOcalStorage=JSON.parse(
+        localStorage.getItem('articles')
+      )
+      if(articlesFromLOcalStorage){
+        setAllArticles(articlesFromLOcalStorage);
+      }
+
+     },[]);
+
+     const handleSubmit= async(e)=>{
+         e.preventDefault();
+      const {data}= await getSummary({articleUrl:article.url});
+      if(data?.summary){
+        const newArticle ={...article,summary:data.summary};
+        const updatedAllArticles=[newArticle,...allArticles];
+        setArticle(newArticle);
+        setAllArticles(updatedAllArticles);
+        
+        localStorage.setItem('articles',JSON.stringify(updatedAllArticles));
+
+        console.log(newArticle);
+      }
+      
+
+      
+     }
+
+     
+
+
+
+
+  return (
+    <section className='mt-16 w-full max-w-xl'>
+      <div className='flex flex-col w-full hap-2'>
+
+        <form action="" className='relative flex justify-center items-center ' onSubmit={handleSubmit}  >
+         
+         <img src={linkIcon} alt="linkicon" className='absolute left-0 my-2 ml-3 w-5' />
+           
+           <input type="url" placeholder='Enter a url' value={article.url} onChange={(e)=>setArticle({...article,url:e.target.value})} required className='url_input peer' />
+          
+          <button type='submit'className='submit_btn peer-focus:border-gray-700 perr-focus:text:gray-700' ><p>↵</p></button>
+          
+        </form>
+          
+          {/* browse url history */}
+          <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
+          {allArticles.reverse().map((item, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={() => setArticle(item)}
+              className='link_card'
+            >
+              
+              <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
+                {item.url}
+              </p>
+            </div>
+          ))}
+        </div>
+
+         </div>
+        
+        {/* display reasults */}
+        <div className='my-10 max-w-full flex justify-center items-center'>
+        {isFetching ? (
+          <img src={loader} alt='loader' className='w-20 h-20 object-contain' />
+        ) : error ? (
+          <p className='font-inter font-bold text-black text-center'>
+            Well, that wasn't supposed to happen...
+            <br />
+            <span className='font-satoshi font-normal text-gray-700'>
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className='flex flex-col gap-3'>
+              <h2 className='font-satoshi font-bold text-gray-600 text-xl'>
+                Article <span className='blue_gradient'>Summary</span>
+              </h2>
+              <div className='summary_box'>
+                <p className='font-inter font-medium text-sm text-gray-700'>
+                  {article.summary}
+                </p>
+              </div>
+            </div>
+          )
+        )}
+      </div>
+
+    </section>
+  )
+}
+
+export default Demo
